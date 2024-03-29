c14739dad4fd214d4e01f06b1f0d2721db933951
yusufemiral <100554355+yusufemiral@users.noreply.github.com>
2024-03-29 07:57:04 +0300

    Add files via upload

---
+++ Store/StoreApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index ()
+        {
+            return View ();
+
+        }
+
+    }
+    
+}
---
+++ Store/StoreApp/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace StoreApp.Models
+{
+
+    public class Product
+    {
+         
+         public int ProductId { get; set; }
+         public String? ProductName { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+    
+}
